2caeee0ab10a6557c624dd69e52f4f6c8d2f231b
jay-bailey <salivanth@gmail.com>
2023-07-17 15:38:36 +1000

    Added tests, not yet working. Fixed an error in ablation head tests.

--- tests/acceptance/test_dt_train.py
+++ tests/acceptance/test_dt_train.py
@@ -1,11 +1,14 @@
 import copy
+import numpy as np
+import os
+import pickle
 
 import pytest
 import torch
 from torch.utils.data import DataLoader, random_split
 from torch.utils.data.sampler import WeightedRandomSampler
 
-from src.config import EnvironmentConfig, TransformerModelConfig
+from src.config import EnvironmentConfig, OnlineTrainConfig, RunConfig, TransformerModelConfig
 from src.decision_transformer.offline_dataset import TrajectoryDataset
 from src.decision_transformer.train import test
 from src.decision_transformer.eval import evaluate_dt_agent
@@ -14,7 +17,9 @@ from src.models.trajectory_transformer import (
     CloneTransformer,
     DecisionTransformer,
 )
+from src.utils.trajectory_writer import TrajectoryWriter
 
+import tests.unit.test_utils as test_utils
 
 @pytest.fixture
 def trajectory_data_set():
@@ -25,6 +30,17 @@ def trajectory_data_set():
     )
     return trajectory_data_set
 
+@pytest.fixture
+def run_config():
+    run_config = RunConfig(
+        exp_name="test",
+        seed=1,
+        track=False,
+        wandb_project_name="test",
+        wandb_entity="test",
+    )
+
+    return run_config
 
 @pytest.fixture
 def environment_config(trajectory_data_set):
@@ -41,6 +57,30 @@ def environment_config(trajectory_data_set):
 
     return environment_config
 
+@pytest.fixture
+def online_config():
+    online_config = OnlineTrainConfig(
+        use_trajectory_model=False,
+        hidden_size=64,
+        total_timesteps=180000,
+        learning_rate=0.00025,
+        decay_lr=False,
+        num_envs=4,
+        num_steps=128,
+        gamma=0.99,
+        gae_lambda=0.95,
+        num_minibatches=4,
+        update_epochs=4,
+        clip_coef=0.4,
+        ent_coef=0.2,
+        vf_coef=0.5,
+        max_grad_norm=2,
+        trajectory_path=None,
+        fully_observed=False,
+        device=torch.device("cpu"),
+    )
+
+    return online_config
 
 @pytest.fixture
 def transformer_model_config():
@@ -207,3 +247,67 @@ def test_evaluate_dt_agent(environment_config, dt):
     assert statistics["prop_positive_reward"] == 0.0
     # traj length approx 10
     assert statistics["mean_traj_length"] == pytest.approx(10.0, 1.0)
+
+
+@pytest.mark.parametrize(
+    "dt",
+    [
+        pytest.param("dt1 nctx = 2"),
+        pytest.param("dt2 nctx = 5"),
+        pytest.param("dt3 nctx = 8"),
+    ],
+    indirect=True,
+)
+def test_evaluate_dt_agent_with_trajectory_writer(environment_config, dt, run_config, online_config):
+    trajectory_path = "tmp/test_trajectory_writer_writer.pkl"
+    try:
+        environment_config.max_steps = 10  # speed up test
+        batch = 0
+        eval_env_func = make_env(
+            environment_config,
+            seed=batch,
+            idx=0,
+            run_name=f"dt_eval_videos_{batch}",
+        )
+
+        trajectory_writer = TrajectoryWriter(
+            path=trajectory_path,
+            run_config=run_config,
+            environment_config=environment_config,
+            online_config=online_config,
+            model_config=None,
+        )
+
+        statistics = evaluate_dt_agent(
+            env_id=environment_config.env_id,
+            model=dt,
+            env_func=eval_env_func,
+            track=False,
+            initial_rtg=1,
+            trajectories=10,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+            trajectory_writer=trajectory_writer
+        )
+
+        assert os.path.getsize(trajectory_path) > 0
+        with open(trajectory_path, "rb") as f:
+            data = pickle.load(f)
+            obs = data["data"]["observations"]
+            assert type(obs) == np.ndarray
+            assert obs.dtype == np.float64
+            # assert obs.shape == [10, 10, 20, 20, 7], f"obs.shape is {obs.shape}"
+
+            rewards = data["data"]["rewards"]
+            assert type(rewards) == np.ndarray
+            assert rewards.dtype == np.float64
+            assert rewards.shape == [10, 10, 1], f"rewards.shape is {rewards.shape}"
+
+            actions = data["data"]["actions"]
+            assert type(actions) == np.ndarray
+            assert actions.dtype == np.int64
+            assert actions.shape == [10, 9, 1], f"actions.shape is {actions.shape}"
+    finally:
+        if os.path.exists(trajectory_path):
+            os.remove(trajectory_path)
+
+
